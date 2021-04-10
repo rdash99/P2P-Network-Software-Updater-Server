684618c3972fcadca96deabbb8e039e05a11a436
rdash99 <pokemonmaster9967@gmail.com>
2021-04-10 21:43:12 +0100

    Update TestingNetwork.py

--- TestingNetwork.py
+++ TestingNetwork.py
@@ -3,10 +3,12 @@ import time
 import MainNode
 
 node = MainNode.MainNode("127.0.0.1", 10001)
+node2 = MainNode.MainNode("127.0.0.1", 10002)
 time.sleep(1)
 
 # Do not forget to start your node!
 node.start()
+node2.start()
 time.sleep(1)
 
 # Connect with another node, otherwise you do not create any network!
@@ -19,3 +21,4 @@ node.send_to_nodes({"message": "Hi there!"})
 time.sleep(5) # Create here your main loop of the application
 
 node.stop()
+node2.stop()
