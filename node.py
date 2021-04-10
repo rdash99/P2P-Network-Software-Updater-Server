333429e95fdfcc7f996c3bd50a4af67da73b287a
rdash99 <pokemonmaster9967@gmail.com>
2021-04-10 15:22:32 +0100

    Create node.py

---
+++ node.py
@@ -0,0 +1,26 @@
+from p2pnetwork.node import Node
+
+class MainNode (Node):
+    # python class constructor
+    def __init__(self, host, port):
+        super(MainNode, self).__init__(host, port, None)
+        print("Main Node Started")
+        
+    # methods to implement functionality:
+    def outbound_node_connected(self, node):
+        print("outbound_node_connected: " + node.id)
+        
+    def inbound_node_connected(self, node):
+        print("inbound_node_connected: " + node.id)
+
+    def inbound_node_disconnected(self, node):
+        print("inbound_node_disconnected: " + node.id)
+
+    def outbound_node_disconnected(self, node):
+        print("outbound_node_disconnected: " + node.id)
+
+    def node_message(self, node, data):
+        print("node_message from " + node.id + ": " + str(data))
+        
+    def node_disconnect_with_outbound_node(self, node):
+        print("node wants to disconnect with oher outbound node: " + node.id)
