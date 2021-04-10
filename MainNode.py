a930bfbec26471585eda79c9a971bac7fe751ec0
rdash99 <pokemonmaster9967@gmail.com>
2021-04-10 17:25:14 +0100

    Create MainNode.py

---
+++ MainNode.py
@@ -0,0 +1,34 @@
+from p2pnetwork.node import Node
+
+class MainNode (Node):
+    # Python class constructor
+    def __init__(self, host, port):
+        super(MainNode, self).__init__(host, port, None)
+
+    def outbound_node_connected(self, connected_node):
+        print("outbound_node_connected: " + connected_node.id)
+        
+    def inbound_node_connected(self, connected_node):
+        print("inbound_node_connected: " + connected_node.id)
+
+    def inbound_node_disconnected(self, connected_node):
+        print("inbound_node_disconnected: " + connected_node.id)
+
+    def outbound_node_disconnected(self, connected_node):
+        print("outbound_node_disconnected: " + connected_node.id)
+
+    def node_message(self, connected_node, data):
+        print("node_message from " + connected_node.id + ": " + str(data))
+        
+    def node_disconnect_with_outbound_node(self, connected_node):
+        print("node wants to disconnect with oher outbound node: " + connected_node.id)
+        
+    def node_request_to_stop(self):
+        print("node is requested to stop!")
+
+    # OPTIONAL
+    # If you need to override the NodeConection as well, you need to
+    # override this method! In this method, you can initiate
+    # you own NodeConnection class.
+    def create_new_connection(self, connection, id, host, port):
+        return MainNodeConnection(self, connection, id, host, port)
