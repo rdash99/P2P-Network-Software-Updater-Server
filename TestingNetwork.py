f12600809f5eaaa7d583321b2dcbbd42b9b6d169
rdash99 <pokemonmaster9967@gmail.com>
2021-04-12 12:27:25 +0100

    Update TestingNetwork.py

--- TestingNetwork.py
+++ TestingNetwork.py
@@ -9,17 +9,17 @@ ip = s.getsockname()[0]
 id = socket.gethostname()
 s.close()
 
-node = MainNode.MainNode("127.0.0.1", 10001, id)
-node2 = MainNode.MainNode("127.0.0.1", 10002, id)
+node = MainNode.MainNode(ip, 10001, id)
+# node2 = MainNode.MainNode("127.0.0.1", 10002, id)
 time.sleep(1)
 
 # Do not forget to start your node!
 node.start()
-node2.start()
+# node2.start()
 time.sleep(1)
 
 # Connect with another node, otherwise you do not create any network!
-node.connect_with_node('127.0.0.1', 10002)
+# node.connect_with_node('127.0.0.1', 10002)
 time.sleep(2)
 
 # Example of sending a message to the nodes (dict).
@@ -28,4 +28,4 @@ node.send_to_nodes({"message": "Hi there!"})
 time.sleep(5) # Create here your main loop of the application
 
 node.stop()
-node2.stop()
+# node2.stop()
